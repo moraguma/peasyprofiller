7d4b23a058ef944d64df776338ce4d07851c3307
moraguma <g170603@dac.unicamp.br>
2024-09-10 19:09:09 -0300

    Finishes setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
-# setup.py
+from setuptools import setup, find_namespace_packages
 
-from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='peasyprofiller',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     version='0.1.0',    
     description='Easy and simple profiller for Python',
     url='https://github.com/moraguma/peasyprofiller',
     author='Moraguma',
     author_email='g170603@dac.unicamp.br',
     license='MIT',
-    packages=["peasyprofiller", "peasyprofiller.tests"],
+    packages=find_namespace_packages(),
     install_requires=[
-        'setuptools>=70.0.0'           
+        'setuptools'           
     ],
 
-		# Full list @ https://pypi.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
